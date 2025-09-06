053b262ec5ae6982d600fce9c2c0b281f66e5d95
Jeswin Johnson <78143624+jeswinJohnson@users.noreply.github.com>
2025-09-06 16:45:51 +0530

    all members

---
+++ app/api/auth/all-memebers.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+
+    const response = await fetch(
+      `https://img-169528297296.us-central1.run.app/api/v1/auth/getAllMemebers`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer {"id": 1, "userName": "svd"}`
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return NextResponse.json({ error: data.message }, { status: response.status });
+    }
+
+    return NextResponse.json({ success: true, data });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
